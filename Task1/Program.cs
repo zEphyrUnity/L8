910221701cf4f19c9814abf646071db74aab8109
Igor <61108251+zEphyrUnity@users.noreply.github.com>
2020-03-18 00:18:00 +0300

    Add files via upload

---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+
+/* Папкин Игорь
+ * С помощью рефлексии выведите все свойства структуры DateTime*/
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Type dateTimeProperties = typeof(DateTime);
+            PropertyInfo[] propertyInfos = dateTimeProperties.GetProperties();
+
+            foreach(PropertyInfo pi in propertyInfos)
+            {
+                Console.WriteLine($"{pi}");
+            }
+        }
+    }
+}
